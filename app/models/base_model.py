7243c65354fea3d20d677cca3d38807f1cbde698
andy-shi88 <shi88.andy@gmail.com>
2017-07-31 17:46:13 +0700

    #12 fix wrong vars name

--- app/models/base_model.py
+++ app/models/base_model.py
@@ -7,6 +7,6 @@ class BaseModel:
 	def as_dict(self):
 		''' return readable field if set, and all field otherwise '''
 		if hasattr(self, 'readable'):
-			return {c: getattr(self, c) for c in self.readable}
+			return {c: getattr(self, c) for c in self.visible}
 		else:
 			return {c.name: getattr(self, c.name) for c in self.__table__.columns}
