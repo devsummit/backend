d1ea5f3bca24a95169feef3f6cfbf9a8e7540f31
andy-shi88 <shi88.andy@gmail.com>
2017-07-31 19:24:28 +0700

    #12 add authentication methods

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,9 @@
+from flask import current_app, jsonify
+
 import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
 
 # import classes
 from app.models.base_model import BaseModel
@@ -13,10 +18,33 @@ class User(db.Model, BaseModel):
 
 	# columns definitions
 	id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String)
-    last_name = db.Column(db.String)
-    username = db.Column(db.String, index=True, unique=True)
-    email = db.Column(db.String, index=True, unique=True)
-    password = db.Column(db.String)
-    created_at = db.Column(db.DateTime)
-    updated_at = db.Column(db.DateTime)
+	first_name = db.Column(db.String)
+	last_name = db.Column(db.String)
+	username = db.Column(db.String, index=True, unique=True)
+	email = db.Column(db.String, index=True, unique=True)
+	password = db.Column(db.String)
+	created_at = db.Column(db.DateTime)
+	updated_at = db.Column(db.DateTime)
+
+	def hash_password(self, password):
+		self.password = generate_password_hash(password)
+
+	def verify_password(self, password):
+		return self.password == password
+
+	def generate_auth_token(self, expiration = 6000):
+		s = Serializer(current_app.config['SECRET_KEY'], expires_in = expiration)
+		return s.dumps({'id': self.id})
+
+	@staticmethod
+	def verify_auth_token(token):
+		s = Serializer(current_app.config['SECRET_KEY'])
+		print(s.loads(token))
+		try:
+			data = s.loads(token)
+		except SignatureExpired:
+			return None
+		except BadSignature:
+			return None
+		user = db.session.query(User).filter_by(id = data['id']).first()
+		return user
