a2df2b454e349d936b863bd9e28d0ba767d268fe
andy-shi88 <shi88.andy@gmail.com>
2017-07-31 17:33:08 +0700

    #12 add base model class

---
+++ app/models/base_model.py
@@ -0,0 +1,12 @@
+class BaseModel:
+	''' Base of all model class contains helper method to simplify jobs '''
+
+	'''
+	method to return model class as dict
+	'''
+	def as_dict(self):
+		''' return readable field if set, and all field otherwise '''
+		if hasattr(self, 'readable'):
+			return {c: getattr(self, c) for c in self.readable}
+		else:
+			return {c.name: getattr(self, c.name) for c in self.__table__.columns}
