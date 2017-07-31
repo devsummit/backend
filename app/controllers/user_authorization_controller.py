1659393aa57cc7dc4af2c6c111fc89777eb8cf40
andy-shi88 <shi88.andy@gmail.com>
2017-07-31 21:44:23 +0700

    #12 add handle method for login and save generated access token

--- app/controllers/user_authorization_controller.py
+++ app/controllers/user_authorization_controller.py
@@ -7,6 +7,7 @@ from flask import request, jsonify, json, current_app
 from app.controllers.base_controller import BaseController
 from app.models import db
 from app.models.user import User
+from app.models.access_token import AccessToken
 
 class UserAuthorizationController(BaseController):
 
@@ -19,10 +20,25 @@ class UserAuthorizationController(BaseController):
 			user = db.session.query(User).filter_by(username = username).first()
 			if user is not None:
 				if user.verify_password(password):
-					token = user.generate_auth_token()
-					return BaseController.send_response({'access_token': token.decode()}, 'User logged in successfully')
+					refresh_token = user.generate_refresh_token()
+					access_token = user.generate_auth_token()
+					UserAuthorizationController.save_token(access_token, refresh_token, user.id)
+					return BaseController.send_response({'access_token': access_token.decode(), 'refresh_token': refresh_token}, 'User logged in successfully')
 				else:
 					return BaseController.send_response(None, 'wrong credentials')
 			else:
 				return BaseController.send_response(None, 'username not found')
 		return BaseController.send_response({}, 'username and password required')
+
+	@staticmethod
+	def save_token(access_token, refresh_token, user_id):
+		token_exist = db.session.query(AccessToken).filter_by(user_id=user_id).first()
+		if not token_exist:
+			payload = AccessToken(access_token, refresh_token, user_id)
+			db.session.add(payload)
+			db.session.commit()
+			return
+		token_exist.access_token = access_token
+		token_exist.refresh_token = refresh_token
+		db.session.commit()
+		return
