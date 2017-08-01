ecceb6eba50379abc04a729c9084934c6eca283e
erdivartanovich <erdivartanovich@gmail.com>
2017-08-01 19:43:11 +0700

    add spots table migration

---
+++ migrations/versions/cf5c0ef7ceaa_create_table_spots.py
@@ -0,0 +1,30 @@
+"""create table spots
+
+Revision ID: cf5c0ef7ceaa
+Revises: efa23ae7975d
+Create Date: 2017-08-01 19:13:03.381935
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cf5c0ef7ceaa'
+down_revision = 'efa23ae7975d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('spots',
+		sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('beacon_id', sa.Integer, sa.ForeignKey('beacons.id', ondelete='CASCADE')),
+        sa.Column('stage_id', sa.Integer, sa.ForeignKey('stages.id', ondelete='CASCADE')),
+		sa.Column('timestamps', sa.DateTime)
+    )
+
+
+def downgrade():
+    op.drop_table('spots')
+
