75e981176c40b3a12bb11ad516ee6da0f808e509
andy-shi88 <shi88.andy@gmail.com>
2017-08-07 21:46:43 +0700

    #21 instantiate order and order details service

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -4,6 +4,8 @@ from app.services.ticket_service import TicketService
 from app.services.stage_service import StageService
 from app.services.beacon_service import BeaconService
 from app.services.spot_service import SpotService
+from app.services.order_service import OrderService
+from app.services.order_details_service import OrderDetailsService
 
 # models import
 from app.models.user import User
@@ -11,6 +13,8 @@ from app.models.ticket import Ticket
 from app.models.stage import Stage
 from app.models.beacon import Beacon
 from app.models.spot import Spot
+from app.models.order import Order
+from app.models.order_details import OrderDetails
 from app.models.access_token import AccessToken
 
 # Intantiate models
@@ -19,6 +23,8 @@ ticket = Ticket()
 stage = Stage()
 beacon = Beacon()
 spot = Spot()
+order = Order()
+order_details = OrderDetails()
 access_token = AccessToken()
 
 # instantiate service
@@ -27,3 +33,5 @@ ticketservice = TicketService(ticket)
 stageservice = StageService(stage)
 beaconservice = BeaconService(beacon)
 spotservice = SpotService(spot)
+orderservice = OrderService(order)
+orderdetailservice = OrderDetailsService(order_details)
