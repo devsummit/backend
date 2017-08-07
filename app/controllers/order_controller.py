71c43b7f8ce1443cf8ae46cdcd323e4a13e3f064
andy-shi88 <shi88.andy@gmail.com>
2017-08-07 21:47:11 +0700

    #21 add order controller

---
+++ app/controllers/order_controller.py
@@ -0,0 +1,41 @@
+from app.controllers.base_controller import BaseController
+from app.models.base_model import BaseModel
+from app.services import orderservice
+
+class OrderController(BaseController):
+
+	@staticmethod
+	def index():
+		orders = orderservice.get()
+		return BaseController.send_response_api(BaseModel.as_list(orders), 'orders retrieved successfully')
+
+	@staticmethod
+	def show(id):
+		order = orderservice.show(id)
+		if order is None:
+			return BaseController.send_error_api(None, 'order not found')
+		return BaseController.send_response_api(order.as_dict(), 'order retrieved successfully')
+
+	@staticmethod
+	def create(request, user_id):
+		order_details = request.json['order_details'] if 'order_details' in request.json else []
+
+		payloads = {
+			'user_id': user_id,
+			'order_details': order_details
+		}
+
+		result = orderservice.create(payloads)
+
+		if not result['error']:
+			return BaseController.send_response_api(result['data'], 'order succesfully created')
+		else:
+			return BaseController.send_error_api(None, result['data'])
+
+	@staticmethod
+	def delete(id):
+		order = orderservice.delete(id)
+		if order['error']:
+			return BaseController.send_response_api(None, 'order not found')
+		return BaseController.send_response_api(None, 'order with id: ' + id + ' has been succesfully deleted')
+
