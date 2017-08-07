7541e8fab05d950787d68c956aebb3c4c812f7d5
andy-shi88 <shi88.andy@gmail.com>
2017-08-07 15:51:01 +0700

    #35 add ticket model

---
+++ app/models/ticket.py
@@ -0,0 +1,24 @@
+import datetime
+
+from app.models import db
+from app.models.base_model import BaseModel
+
+
+class Ticket(db.Model, BaseModel):
+
+	# table name
+	__tablename__ = 'tickets'
+	# displayed fields
+	visible = ['id', 'ticket_type', 'price', 'information', 'created_at', 'updated_at']
+
+	# columns definitions
+	id = db.Column(db.Integer, primary_key=True)
+	ticket_type = db.Column(db.String)
+	price = db.Column(db.Integer)
+	information = db.Column(db.String)
+	created_at = db.Column(db.DateTime)
+	updated_at = db.Column(db.DateTime)
+
+	def __init__(self):
+		self.created_at = datetime.datetime.now()
+		self.updated_at = datetime.datetime.now()
