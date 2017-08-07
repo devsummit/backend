0abaab229d16fa995225a7625b3f7539bd0ed396
andy-shi88 <shi88.andy@gmail.com>
2017-08-07 17:54:46 +0700

    #50 instantiate spot service and model

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -3,12 +3,14 @@ from app.services.user_service import UserService
 from app.services.ticket_service import TicketService
 from app.services.stage_service import StageService
 from app.services.beacon_service import BeaconService
+from app.services.spot_service import SpotService
 
 # models import
 from app.models.user import User
 from app.models.ticket import Ticket
 from app.models.stage import Stage
 from app.models.beacon import Beacon
+from app.models.spot import Spot
 from app.models.access_token import AccessToken
 
 # Intantiate models
@@ -16,6 +18,7 @@ user = User()
 ticket = Ticket()
 stage = Stage()
 beacon = Beacon()
+spot = Spot()
 access_token = AccessToken()
 
 # instantiate service
@@ -23,3 +26,4 @@ userservice = UserService(user, access_token)
 ticketservice = TicketService(ticket)
 stageservice = StageService(stage)
 beaconservice = BeaconService(beacon)
+spotservice = SpotService(spot)
