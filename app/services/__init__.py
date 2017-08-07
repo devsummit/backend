3a746fc6ca89f4b3a033a7423c3b10ebeb78fc8a
andy-shi88 <shi88.andy@gmail.com>
2017-08-07 15:50:11 +0700

    #35 instantiate ticket service

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,9 +1,17 @@
+# services import
 from app.services.user_service import UserService
+from app.services.ticket_service import TicketService
+
+# models import
 from app.models.user import User
+from app.models.ticket import Ticket
 from app.models.access_token import AccessToken
 
-
+# Intantiate models
 user = User()
+ticket = Ticket()
 access_token = AccessToken()
 
+# instantiate service
 userservice = UserService(user, access_token)
+ticketservice = TicketService(ticket)
