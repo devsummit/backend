11e1ebde7d6d7766e20151fe38c9f04645df0c57
erdivartanovich <erdivartanovich@gmail.com>
2017-08-07 20:26:45 +0700

    add complete routes for events

--- app/routes/api/v1/events.py
+++ app/routes/api/v1/events.py
@@ -6,13 +6,23 @@ from app.controllers.event_controller import EventController
 
 events = Blueprint('events', __name__)
 
-BASE = '/events'
-@events.route(BASE, methods=['GET'])
+@events.route('/', methods=['GET'])
 def index():
-	return EventController.index(request)
+	return EventController.index()
 
+@events.route('/<event_id>', methods=['GET'])
+def show(event_id):
+	return EventController.show(event_id)
 
-@events.route(BASE, methods=['PATCH'])
-def edit():
-	return EventController.edit(request)
+@events.route('', methods=['POST'])
+def create():
+	return EventController.create(request)
+
+@events.route('/<event_id>', methods=['PATCH'])
+def update(event_id):
+	return EventController.update(request, event_id)
+
+@events.route('/<event_id>', methods=['DELETE'])
+def delete(event_id):
+	return EventController.delete(event_id)
 
