1cd1a158e98b2245842aca1388ec1ca239d55f76
andy-shi88 <shi88.andy@gmail.com>
2017-08-09 12:19:30 +0700

    #52 add schedule model

---
+++ app/models/schedule.py
@@ -0,0 +1,42 @@
+import datetime
+# import classes
+from app.models.base_model import BaseModel
+from app.models import db
+from app.models.event import Event
+from app.models.stage import Stage
+
+class Schedule(db.Model, BaseModel):
+	# table name
+	__tablename__ = 'schedules'
+	# displayed fields
+	visible = ['id', 'user_id', 'event_id', 'time_start', 'time_end', 'created_at', 'updated_at']
+
+	# columns definitions
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(
+		db.String(40),
+		db.ForeignKey('users.id'),
+		nullable=False
+	)
+	user = db.relationship('User')
+	event_id = db.Column(
+		db.String(40),
+		db.ForeignKey('events.id'),
+		nullable=False
+	)
+	event = db.relationship('Event')
+
+	stage_id = db.Column(
+		db.String(40),
+		db.ForeignKey('stages.id'),
+		nullable=False
+	)
+	stage = db.relationship('Stage')
+	time_start = db.Column(db.DateTime)
+	time_end = db.Column(db.DateTime)
+	created_at = db.Column(db.DateTime)
+	updated_at = db.Column(db.DateTime)
+
+	def __init__(self):
+		self.created_at = datetime.datetime.now()
+		self.updated_at = datetime.datetime.now()
