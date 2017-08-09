c341be68f969709fd6f079af12d6d042a7a64c83
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-08-09 20:30:35 +0700

    #75 add role_id

--- app/models/user.py
+++ app/models/user.py
@@ -15,7 +15,7 @@ class User(db.Model, BaseModel):
 	# table name
 	__tablename__ = 'users'
 	# displayed fields
-	visible = ['id', 'first_name', 'last_name', 'username', 'email', 'created_at', 'updated_at']
+	visible = ['id', 'first_name', 'last_name', 'username', 'email', 'role_id', 'created_at', 'updated_at']
 
 	# columns definitions
 	id = db.Column(db.Integer, primary_key=True)
@@ -24,6 +24,7 @@ class User(db.Model, BaseModel):
 	username = db.Column(db.String, index=True, unique=True)
 	email = db.Column(db.String, index=True, unique=True)
 	password = db.Column(db.String)
+	role_id = db.Column(db.String)
 	created_at = db.Column(db.DateTime)
 	updated_at = db.Column(db.DateTime)
 
