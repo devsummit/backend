b704f80a551476c46b8805c4822e6c157c65b1d3
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-08-09 21:10:40 +0700

    #75 rewrite stages seeder

--- seeders/stages_seeder.py
+++ seeders/stages_seeder.py
@@ -17,19 +17,11 @@ class StagesSeeder():
         """
         fake = Faker()
         stage_types = ['podium', 'booth', 'mainstage']
-        for i in range(0, 10):
-            name = fake.name()
-            stage_type = random.choice(stage_types)
-            information = fake.sentence(
-                nb_words=6, variable_nb_words=True, ext_word_list=None)
-            stage = {
-                'name': name,
-                'stage_type': stage_type,
-                'information': information
-            }     
+        for i in range(0, 10): 
             new_stage = Stage()
-            new_stage.name = stage['name']
-            new_stage.stage_type = stage['stage_type']
-            new_stage.information = stage['information']
+            new_stage.name = fake.name()
+            new_stage.stage_type = random.choice(stage_types)
+            new_stage.information = fake.sentence(
+                nb_words=6, variable_nb_words=True, ext_word_list=None)
             db.session.add(new_stage)
             db.session.commit()
