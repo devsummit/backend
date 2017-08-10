dffdacb1021546ad4db849b4a38a9e81dca077c8
erdivartanovich <erdivartanovich@gmail.com>
2017-08-10 23:25:00 +0700

    fix services

--- app/services/ticket_transfer_service.py
+++ app/services/ticket_transfer_service.py
@@ -8,8 +8,6 @@ from app.models.ticket_transfer_log import TicketTransferLog
 
 class TicketTransferService():
 
-    # query = meta.Session.query(User).filter(or_(User.firstname.like(searchVar),User.lastname.like(searchVar)))
-
     def get_logs(self, user_id=''):
         if user_id == '':
             transferslogs = db.session.query(TicketTransferLog).all()
@@ -20,8 +18,47 @@ class TicketTransferService():
 
         return transferslogs
 
-    def show(self, id):
-        pass
 
-    def create(self, payloads):
-        pass
+    def transfer(self, user_id, user_ticket_id, receiver_id):
+        if (user_id == '' or user_ticket_id==''  or receiver_id==''):
+            return {
+				'error': True,
+				'data': 'sender_id, user_ticket_id or receiver_id is not valid'
+			}
+
+        userticket = db.session.query(UserTicket).filter_by(id=user_ticket_id)
+        if userticket.count()<=0:
+            return {
+                'error': True,
+                'data': 'ticket not found'
+            }
+
+        try:            
+            # update user ticket
+            userticket.update({
+                'user_id': receiver_id,
+                'updated_at': datetime.datetime.now()
+            })
+            db.session.commit()
+
+            # add transfer log
+            transferlog = TicketTransferLog()
+            transferlog.sender_user_id =  user_id
+            transferlog.receiver_user_id =  receiver_id
+            # transferlog.user_ticket_id = 1
+            transferlog.user_ticket_id = user_ticket_id
+            db.session.add(transferlog)
+
+            db.session.commit()
+            data = transferlog.as_dict()
+            return {
+				'error': False,
+				'data': data
+			}
+        except SQLAlchemyError as e:
+            data = e.orig.args
+            return {
+                'error': True,
+                'data': data
+            }
+            
