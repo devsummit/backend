82f9d8cda04b1524f35b3a141aa6768dd111f811
erdivartanovich <erdivartanovich@gmail.com>
2017-08-10 18:47:00 +0700

    add ticket-transer-service boilerplate

--- app/services/ticket_transfer_service.py
+++ app/services/ticket_transfer_service.py
@@ -2,7 +2,19 @@ import datetime
 from app.models import db
 from sqlalchemy.exc import SQLAlchemyError
 # import model class
-from app.models.ticket_transfer import TicketTransfer
+from app.models.user_ticket import UserTicket
+from app.models.ticket_transfer_log import TicketTransferLog
 
 
 class TicketTransferService():
+    
+    def get(self):
+        transferslogs = db.session.query(TicketTransferLog).all()
+        return transferslogs
+
+    def show(self, id):
+        pass
+
+    def create(self, payloads):
+        pass
+    
