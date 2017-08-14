a61023a94752b3a071e8ad493c401bc876c82ba3
andy-shi88 <shi88.andy@gmail.com>
2017-08-14 10:33:56 +0700

    #46 add social sign in method

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,10 +1,14 @@
+import requests
 from app.models import db
 from sqlalchemy.exc import SQLAlchemyError
+from oauth2client import client, crypt
+
 from app.models.access_token import AccessToken
 from app.models.user import User
 from app.models.booth import Booth
 from app.models.attendee import Attendee
 from app.models.speaker import Speaker
+from app.models.client import Client
 from app.configs.constants import ROLE
 
 
@@ -17,7 +21,9 @@ class UserService:
         self.model_user.email = payloads['email']
         self.model_user.username = payloads['username']
         self.model_user.role_id = payloads['role']
+        self.model_user.social_id = payloads['social_id']
         self.model_user.hash_password(payloads['password'])
+
         db.session.add(self.model_user)
         try:
             db.session.commit()
@@ -44,9 +50,51 @@ class UserService:
         self.model_user = db.session.query(User).filter_by(username=username).first()
         return self.model_user
 
-    def save_token(self):
+    def social_sign_in(self, provider, social_token):
+        if (provider == 'google'):
+            # check token integrity
+            try:
+                # get client id
+                CLIENT_ID = db.session.query(Client).filter_by(app_name=provider)
+                idinfo = client.verify_id_token(social_token, CLIENT_ID)
+                # Or, if multiple clients access the backend server:
+                #idinfo = client.verify_id_token(token, None)
+                #if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
+                #    raise crypt.AppIdentityError("Unrecognized client.")
+                if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+                    raise crypt.AppIdentityError("Wrong issuer.")
+                # If auth request is from a G Suite domain:
+                #if idinfo['hd'] != GSUITE_DOMAIN_NAME:
+                #    raise crypt.AppIdentityError("Wrong hosted domain.")
+            except crypt.AppIdentityError:
+                # Invalid token
+                userid = idinfo['sub']
+                # return user social id
+            return userid
+        elif(provider == 'facebook'):
+            # check token integrity
+            try:
+                CLIENT_ID = db.session.query(Client).filter_by(app_name=provider)
+                facebook_endpoint = 'graph.facebook.com/debug_token?input_token=' + social_token + '&access_token=' + CLIENT_ID
+                result = requests.get(facebook_endpoint).content
+                # check for result -> app_id -> user_id
+            except Exception as e:
+                raise
+
+    def check_social_account(self, provider, social_id):
+        # check if social id exist in user table
+        self.model_user = db.session.query(User).filter_by(social_id=social_id).first()
+        if self.model_user is not None:
+            # user with social_id exist
+            # return the user
+            return user
+        else:
+            return None
+
+    def save_token(self, provider='password_grant'):
         token_exist = db.session.query(AccessToken).filter_by(user_id=self.model_user.id).first()
         if not token_exist:
+            self.model_access_token = AccessToken()
             payload = self.model_access_token.init_token(self.model_user.generate_auth_token(), self.model_user.generate_refresh_token(), self.model_user.id)            
             db.session.add(payload)
             db.session.commit()
@@ -56,6 +104,10 @@ class UserService:
             }
         token_exist.access_token = self.model_user.generate_auth_token()
         token_exist.refresh_token = self.model_user.generate_refresh_token()
+        # get id of client app
+        client = db.session.query(Client).filter_by(app_name=provider).first()
+        token_exist.client_id =client.id
+
         db.session.commit()
         return{
             'error': True,
