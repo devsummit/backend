8f96f7cf1fa78ce1ffa5aaa9e87fb5900e5f0993
Hana Alaydrus <hanalaydrus@gmail.com>
2017-08-16 17:42:51 +0700

    #111 add client id column

--- migrations/versions/79d78e481ff5_create_access_token_table.py
+++ migrations/versions/79d78e481ff5_create_access_token_table.py
@@ -8,10 +8,9 @@ Create Date: 2017-07-31 20:54:35.517706
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
-revision = '79d78e481ff5'
-down_revision = '1314685ec5c0'
+revision = 'e431e5b0f9c3'
+down_revision = '55d9b9bc9d9c'
 branch_labels = None
 depends_on = None
 
@@ -22,6 +21,8 @@ def upgrade():
         sa.Column('id', sa.Integer, primary_key=True),
         sa.Column('user_id', sa.Integer,
             sa.ForeignKey('users.id', ondelete='CASCADE')),
+        sa.Column('client_id', sa.Integer,
+            sa.ForeignKey('clients.id', ondelete='CASCADE')),
         sa.Column('access_token', sa.String),
         sa.Column('refresh_token', sa.String),
         sa.Column('created_at', sa.DateTime),
