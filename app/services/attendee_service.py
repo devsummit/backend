ff8296fe81f8b8dbd10e278bbf70945829d4e80a
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-08-16 18:06:00 +0700

    #109 fix attendee service lint

--- app/services/attendee_service.py
+++ app/services/attendee_service.py
@@ -1,6 +1,4 @@
-import datetime
 from app.models import db
-from sqlalchemy.exc import SQLAlchemyError
 # import model class
 from app.models.attendee import Attendee
 
@@ -9,7 +7,7 @@ class AttendeeService():
     def get(self):
         attendees = db.session.query(Attendee).all()
         return attendees
-    
+
     def show(self, id):
         attendee = db.session.query(Attendee).filter_by(id=id).first()
         return attendee
