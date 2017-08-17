0c2bf8715792aec17454c3abe882aca18998298f
caudaganesh <caudaganesh@gmail.com>
2017-08-17 17:53:00 +0700

    include user to attendee api respond

--- app/services/attendee_service.py
+++ app/services/attendee_service.py
@@ -1,13 +1,20 @@
 from app.models import db
 # import model class
 from app.models.attendee import Attendee
+from app.models.user import User
 
 
 class AttendeeService():
     def get(self):
         attendees = db.session.query(Attendee).all()
-        return attendees
+        _attendees = []
+        for attendee in attendees:
+            attendee = attendee.as_dict()
+            attendee['user'] = db.session.query(User).filter_by(id=attendee['user_id']).first().as_dict()
+            _attendees.append(attendee)
+        return _attendees
 
     def show(self, id):
-        attendee = db.session.query(Attendee).filter_by(id=id).first()
+        attendee = db.session.query(Attendee).filter_by(id=id).first().as_dict()
+        attendee['user'] = db.session.query(User).filter_by(id=attendee['user_id']).first().as_dict()
         return attendee
