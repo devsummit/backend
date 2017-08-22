d932acdb182d23fc0867b580da82291d84acc8c9
kristoforusrp <kristoforusrp@gmail.com>
2017-08-22 14:15:32 +0700

    add payment seeder

---
+++ seeders/payments_seeder.py
@@ -0,0 +1,42 @@
+from faker import Faker
+from random import randint
+from app.models.base_models import BaseModel
+from app.models.payment import Payment
+from app.models.order import Order
+
+'''
+Seeder class for
+'''
+
+
+class PaymentsSeeder():
+
+    @staticmethod
+    def run():
+        """
+        Create 10 Payments seeds
+        """
+        fake = Faker()
+        orders = BaseModel.as_list(db.session.query(Order)).all())
+        range_start = 10 ** ( 7 - 1 )
+        range_end = ( 10 ** 7 ) - 1
+
+        for i in range(0, 10):
+            card_id = randint(range_start, range_end)
+            total = randint(range_start, range_end)
+            additional_information = fake.sentence(
+                    nb_words=10, variable_nb_words=True, ext_word_list=None
+            )
+            sender = fake.sentence(
+                    nb_words=6, variable_nb_words=True, ext_word_list=None
+            )
+            order_id = random.choice(orders)['id']
+            new_payment = Payment()
+            new_payment.card_id = card_id
+            new_payment.total = total
+            new_payment.additional_information = additional_information
+            new_payment.sender = sender
+            db.session.add(new_payment)
+            db.session.commit()
+
+
