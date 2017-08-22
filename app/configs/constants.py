ff2ccfc5c9a4121f445951db8ae37fd307937a61
kristoforusrp <kristoforusrp@gmail.com>
2017-08-23 06:59:04 +0700

    add constant for midtrans

--- app/configs/constants.py
+++ app/configs/constants.py
@@ -4,3 +4,9 @@ ROLE = {
     'booth': 3,
     'speaker': 4 
 }
+
+MIDTRANS_API_BASE_URL = 'https://api.sanbox.midtrans.com/v2/'
+# Change these consts to devsummit later
+MERCHANT_ID = 'M1066775'
+CLIENT_KEY = 'VT-client-g8cB-IVLwe64YIdv'
+SERVER_KEY = 'VT-server-njhqghnFUZbtZgOg9ldNtY0l:'
