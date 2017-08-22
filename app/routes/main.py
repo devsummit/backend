9bbc493403eac8c8c1cc781b692a908cb0a9457b
erdivartanovich <erdivartanovich@gmail.com>
2017-08-23 00:03:56 +0700

    #124 add login page

--- app/routes/main.py
+++ app/routes/main.py
@@ -6,4 +6,9 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('resources/views/admin/base/index.html', entries='hellooooooo')
+    return render_template('resources/views/admin/base/index.html')
+
+@main.route('/login')
+def login():
+    return render_template('resources/views/admin/auth/login.html')
+    
