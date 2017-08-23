255f9d31bfa418eabf28425299c21e572596e7a4
kristoforusrp <kristoforusrp@gmail.com>
2017-08-23 12:06:27 +0700

    fix constant typo

--- app/configs/constants.py
+++ app/configs/constants.py
@@ -5,7 +5,7 @@ ROLE = {
     'speaker': 4 
 }
 
-MIDTRANS_API_BASE_URL = 'https://api.sanbox.midtrans.com/v2/'
+MIDTRANS_API_BASE_URL = 'https://api.sandbox.midtrans.com/v2/'
 # Change these consts to devsummit later
 MERCHANT_ID = 'M1066775'
 CLIENT_KEY = 'VT-client-g8cB-IVLwe64YIdv'
