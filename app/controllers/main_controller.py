70dc607810ef17b38da7be12448e94e0134a4bae
erdivartanovich <erdivartanovich@gmail.com>
2017-08-23 23:02:25 +0700

    init attendee list page

--- app/controllers/main_controller.py
+++ app/controllers/main_controller.py
@@ -1,7 +1,11 @@
 from flask import render_template
 from app.controllers.base_controller import BaseController
-
+from app.services import attendeeservice
 
 class MainController(BaseController):
     def index():
-        return render_template('resources/views/admin/base/index.html')
+        return render_template('admin/base/index.html')
+
+    def getAttendees():
+        attendees = attendeeservice.get()
+        return render_template('admin/attendees.html', attendees=attendees)
