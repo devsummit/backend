c8437adb7fcade24aa4217be00fe96c0bdc7b9f4
andy-shi88 <shi88.andy@gmail.com>
2017-08-23 18:21:58 +0700

    #36 add credit card payment

--- app/controllers/payment_controller.py
+++ app/controllers/payment_controller.py
@@ -7,98 +7,141 @@ class PaymentController(BaseController):
 
     @staticmethod
     def create(request):
-
         payment_type = request.json['payment_type'] if 'payment_type' in request.json else None
         gross_amount = request.json['gross_amount'] if 'gross_amount' in request.json else None
         bank = request.json['bank'] if 'bank' in request.json else None
         order_id = request.json['order_id'] if 'order_id' in request.json else None
 
-        if (bank == 'permata'):
-            if payment_type and gross_amount and bank and order_id: 
+        if(payment_type and payment_type == 'bank_transfer'):
+
+            if (bank == 'permata'):
+                if payment_type and gross_amount and bank and order_id: 
+                    payloads = {
+                        'payment_type': payment_type,
+                        'gross_amount': gross_amount,
+                        'bank': bank,
+                        'order_id': order_id
+                    }
+                else:
+                    return BaseController.send_error_api(None, 'field is not complete')
+
+                result = paymentservice.bank_transfer(payloads)
+
+                if result['status_code'] == '201':
+                    return BaseController.send_response_api(result, 'Succesfully')
+                else:
+                    return BaseController.send_error_api(None, result)
+
+            if (bank == 'bca'):
+                email = request.json['email'] if 'email' in request.json else None
+                first_name = request.json['first_name'] if 'first_name' in request.json else None
+                last_name = request.json['last_name'] if 'last_name' in request.json else None
+                phone = request.json['phone'] if 'phone' in request.json else None
+                va_number = request.json['va_number'] if 'va_number' in request.json else None
+
+                if payment_type and gross_amount and order_id and email and first_name and last_name and phone and bank and va_number:
+                    payloads = {
+                        'payment_type': payment_type,
+                        'gross_amount': gross_amount,
+                        'order_id': order_id,
+                        'email': email,
+                        'first_name': first_name,
+                        'last_name': last_name,
+                        'phone': phone,
+                        'bank': bank,
+                        'va_number': va_number
+                    }
+                else:
+                    return BaseController.send_error_api(None, 'field is not complete')
+                
+                result = paymentservice.bank_transfer(payloads)
+
+                if result['status_code'] == '201':
+                    return BaseController.send_response_api(result, 'Succesfully')
+                else:
+                    return BaseController.send_error_api(None, result)
+
+            if(bank == 'bni'):
+                email = request.json['email'] if 'email' in request.json else None
+                first_name = request.json['first_name'] if 'first_name' in request.json else None
+                last_name = request.json['last_name'] if 'last_name' in request.json else ''
+                phone = request.json['phone'] if 'phone' in request.json else None
+                va_number = request.json['va_number'] if 'va_number' in request.json else None
+                if email and first_name and last_name and phone and va_number:
+                    payloads = {
+                        'payment_type': payment_type,
+                        'gross_amount': gross_amount,
+                        'order_id': order_id,
+                        'email': email,
+                        'first_name': first_name,
+                        'last_name': last_name,
+                        'phone': phone,
+                        'bank': bank,
+                        'va_number': va_number
+                    }
+                else:
+                    return BaseController.send_error_api(None, 'field is not complete')
+
+                result = paymentservice.bank_transfer(payloads)
+
+                if not result['status_code'] == '201':
+                    return BaseController.send_response_api(result, 'bank transfer transaction is created')
+                else:
+                    return BaseController.send_error_api(None, result)
+
+            if(bank == 'mandiri_bill'):
                 payloads = {
-                    'payment_type': payment_type,
-                    'gross_amount': gross_amount,
+                    'payment_type': 'echannel',
                     'bank': bank,
-                    'order_id': order_id
-                }
-            else:
-                return BaseController.send_error_api(None, 'field is not complete')
-
-            result = paymentservice.bank_transfer(payloads)
-
-            if result['status_code'] == '201':
-                return BaseController.send_response_api(result, 'Succesfully')
-            else:
-                return BaseController.send_error_api(None, result)
-
-        if (bank == 'bca'):
-            email = request.json['email'] if 'email' in request.json else None
-            first_name = request.json['first_name'] if 'first_name' in request.json else None
-            last_name = request.json['last_name'] if 'last_name' in request.json else None
-            phone = request.json['phone'] if 'phone' in request.json else None
-            va_number = request.json['va_number'] if 'va_number' in request.json else None
-
-            if payment_type and gross_amount and order_id and email and first_name and last_name and phone and bank and va_number:
-                payloads = {
-                    'payment_type': payment_type,
                     'gross_amount': gross_amount,
                     'order_id': order_id,
-                    'email': email,
-                    'first_name': first_name,
-                    'last_name': last_name,
-                    'phone': phone,
-                    'bank': bank,
-                    'va_number': va_number
                 }
-            else:
-                return BaseController.send_error_api(None, 'field is not complete')
-            
-            result = paymentservice.bank_transfer(payloads)
 
-            if result['status_code'] == '201':
-                return BaseController.send_response_api(result, 'Succesfully')
-            else:
-                return BaseController.send_error_api(None, result)
+                result = paymentservice.bank_transfer(payloads)
 
-        if(bank == 'bni'):
-            email = request.json['email'] if 'email' in request.json else None
-            first_name = request.json['first_name'] if 'first_name' in request.json else None
-            last_name = request.json['last_name'] if 'last_name' in request.json else ''
-            phone = request.json['phone'] if 'phone' in request.json else None
-            va_number = request.json['va_number'] if 'va_number' in request.json else None
-            if email and first_name and last_name and phone and va_number:
-                payloads = {
-                    'payment_type': payment_type,
-                    'gross_amount': gross_amount,
-                    'order_id': order_id,
-                    'email': email,
-                    'first_name': first_name,
-                    'last_name': last_name,
-                    'phone': phone,
-                    'bank': bank,
-                    'va_number': va_number
-                }
-            else:
-                return BaseController.send_error_api(None, 'field is not complete')
+                if not result['status_code'] == '201':
+                    return BaseController.send_response_api(result, 'bank transfer transaction is created')
+                else:
+                    return BaseController.send_error_api(None, result)
+        else:
+            if gross_amount and order_id:
+                email = request.json['email'] if 'email' in request.json else None
+                first_name = request.json['first_name'] if 'first_name' in request.json else None
+                last_name = request.json['last_name'] if 'last_name' in request.json else ''
+                phone = request.json['phone'] if 'phone' in request.json else None
+                gross_amount = request.json['gross_amount'] if 'gross_amount' in request.json else None
+                card_number = request.json['card_number'] if 'card_number' in request.json else None
+                card_exp_month = request.json['card_exp_month'] if 'card_exp_month' in request.json else None
+                card_exp_year = request.json['card_exp_year'] if 'card_exp_year' in request.json else None
+                card_cvv = request.json['card_cvv'] if 'card_cvv' in request.json else None
+                bank = request.json['bank'] if 'bank' in request.json else None
+                client_key = request.json['client_key'] if 'client_key' in request.json else None
+                billing_address = request.json['billing_address'] if 'billing_address' in request.json else None
+                shipping_address = request.json['shipping_address'] if 'shipping_address' in request.json else None
 
-            result = paymentservice.bank_transfer(payloads)
+                if email and first_name and last_name and phone and billing_address and shipping_address:
+                    payloads = {
+                        'gross_amount': gross_amount,
+                        'order_id': order_id,
+                        'email': email,
+                        'first_name': first_name,
+                        'last_name': last_name,
+                        'phone': phone,
+                        'card_number': card_number,
+                        'card_exp_month': card_exp_month,
+                        'card_exp_year': card_exp_year,
+                        'card_cvv': card_cvv,
+                        'client_key': client_key,
+                        'bank': bank,
+                        'billing_address': billing_address,
+                        'shipping_address': shipping_address
+                    }
+                else:
+                    return BaseController.send_error_api(None, 'field is not complete')
 
-            if not result['status_code'] == '201':
-                return BaseController.send_response_api(result, 'bank transfer transaction is created')
-            else:
-                return BaseController.send_error_api(None, result)
+                result = paymentservice.credit_payment(payloads)
 
-        if(bank == 'mandiri_bill'):
-            payloads = {
-                'payment_type': 'echannel',
-                'bank': bank,
-                'gross_amount': gross_amount,
-                'order_id': order_id,
-            }
-
-            result = paymentservice.bank_transfer(payloads)
-
-            if not result['status_code'] == '201':
-                return BaseController.send_response_api(result, 'bank transfer transaction is created')
-            else:
-                return BaseController.send_error_api(None, result)
+                if not result['status_code'] == '201':
+                    return BaseController.send_response_api(result, 'bank transfer transaction is created')
+                else:
+                    return BaseController.send_error_api(None, result)
--- app/services/payment_service.py
+++ app/services/payment_service.py
@@ -13,6 +13,78 @@ from app.models.base_model import BaseModel
 
 class PaymentService():
 
+    def credit_payment(self, payloads):
+
+        self.authorization = base64.b64encode(bytes(SERVER_KEY, 'utf-8')).decode()
+
+        headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': self.authorization}
+        # generate order details payload
+        details = self.get_order_details(payloads['order_id'])    
+        # register and get credit card token 
+        token = self.get_credit_token(payloads)
+
+        data = {}
+        data['payment_type'] = 'credit_card'
+        data['transaction_detail'] = {}
+        data['transaction_detail']['order_id'] = payloads['order_id']
+        data['transaction_detail']['gross_amount'] = payloads['gross_amount']
+        data['credit_card'] = {}
+        data['credit_card']['token_id'] = token
+        data['item_details'] = details
+        data['customer_details'] = {}
+        data['customer_details']['first_name'] = payloads['first_name']
+        data['customer_details']['last_name'] = payloads['last_name']
+        data['customer_details']['email'] = payloads['email']
+        data['customer_details']['phone'] = payloads['phone']
+        data['customer_details']['billing_address'] = payloads['billing_address']
+        data['customer_details']['shipping_address'] = payloads['shipping_address']
+        data['transaction_details'] = {}
+        data['transaction_details']['order_id'] = payloads['order_id']
+        data['transaction_details']['gross_amount'] = payloads['gross_amount']
+        
+        try:
+            endpoint = url + 'charge'
+            result = requests.post(
+                    endpoint,
+                    headers={
+                        'Accept': 'application/json',
+                        'Content-Type':'application/json', 
+                        'Authorization': 'Basic ' + self.authorization
+                    }, json=data
+            )
+
+            payload = result.json()
+            print(payload)
+            if (payload['status_code'] == '201'):
+                self.save_payload(payload)
+
+            return payload
+
+        except Exception as e:
+            # Invalid payloads
+            return None
+
+
+
+    def get_credit_token(self, payloads):
+        endpoint = 'https://api.sandbox.midtrans.com/v2/card/register?'
+        card_number = 'card_number=' + payloads['card_number']
+        card_exp_month = '&card_exp_month=' + payloads['card_exp_month']
+        card_exp_year= '&card_exp_year=' + payloads['card_exp_year'] 
+        card_cvv = '&card_cvv=' + payloads['card_cvv']
+        bank = '&bank=' + payloads['bank']
+        gross_amount= '&gross_amount=' + str(payloads['gross_amount'])
+        client_key = '&client_key=' + payloads['client_key']
+        result_endpoint = endpoint + card_number + card_exp_month + card_exp_year + card_cvv + bank + '&secure=true' + gross_amount + gross_amount + client_key
+        try:
+            headers = {'Accept': 'application/json', 'Content-Type':'application/json'}
+            result = requests.get(result_endpoint, headers=headers)
+            res = result.json()
+            return res['saved_token_id']
+        except Exception as e:
+            # Invalid payloads
+            return None
+
     def bank_transfer(self, payloads):
         
         payloads['gross_amount'] = int(payloads['gross_amount'])
@@ -167,9 +239,7 @@ class PaymentService():
         except Exception as e:
             # Invalid payloads
             return None
-            data['transaction_details'] = {}
-            data['transaction_details']['order_id'] = payloads['order_id']
-            data['transaction_details']['gross_amount'] = payloads['gross_amount']
+            
 
     def get_order_details(self, order_id):
         # using order_id to get ticket_id, price, quantity, ticket_type(name) in payment service
@@ -199,6 +269,3 @@ class PaymentService():
 
         db.session.add(new_payment)
         db.session.commit()
-
-
-
