80143b1ee2ff595aded17b40de0efce7fb08da96
andy-shi88 <shi88.andy@gmail.com>
2017-09-06 20:11:31 +0700

    #188 include reserved, paid, and available data to ticket list

--- app/services/ticket_service.py
+++ app/services/ticket_service.py
@@ -3,13 +3,71 @@ from app.models import db
 from sqlalchemy.exc import SQLAlchemyError
 # import model class
 from app.models.ticket import Ticket
+from app.models.payment import Payment
+from app.models.order_details import OrderDetails
+from app.configs.constants import SLOT
 
 
 class TicketService():
 
+	def get_items(self, payment):
+		result = [0, 0, 0, 0, 0]
+		order_details = db.session.query(OrderDetails).filter_by(order_id=payment['order_id']).all()
+		for item in order_details:
+			data = item.as_dict()
+			result[data['ticket_id'] - 1] += data['count']
+		return result
+
+	def count_ticket_left(self, payments):
+		reserved = [0, 0, 0, 0, 0]
+		paid = [0, 0, 0, 0, 0]
+		for payment in payments:
+			data = payment.as_dict()
+			if data['transaction_status'] is 'deny':
+				continue
+
+			count_data = self.get_items(data)
+			for i in range(0, 5):
+				if data['transaction_status'] == 'capture':
+					paid[i] += count_data[i]
+				else:
+					reserved[i] += count_data[i]
+
+		return {'reserved': reserved, 'paid': paid}
+
+	def include_ticket_left(self, tickets):
+		total_amount = [SLOT['commercial'], SLOT['commercial'], SLOT['commercial'], SLOT['commercial'], SLOT['community']]
+		payments = db.session.query(Payment).all()
+		data = self.count_ticket_left(payments)
+		for i in range(0, 5):
+			total_amount[i] -= data['reserved'][i]
+			total_amount[i] -= data['paid'][i]
+		return {
+			'total_amount': total_amount, 
+			'reserved': data['reserved'],
+			'paid': data['paid']
+		} 
+
 	def get(self):
 		tickets = db.session.query(Ticket).all()
-		return tickets
+		_results = []
+		for ticket in tickets:
+			data = ticket.as_dict()
+			_results.append(data)
+		count_data = self.include_ticket_left(_results)
+		_results = self.include_count_data(count_data, _results)
+		return {
+			'error': False,
+			'data': _results,
+			'message': 'Ticket retrieved succesfully'
+		}
+
+	def include_count_data(self, count_data, results):
+		for i in range(0, 5):
+			results[i]['reserved'] = count_data['reserved'][i]
+			results[i]['paid'] = count_data['paid'][i]
+			results[i]['available'] = count_data['total_amount'][i]
+		return results
 
 	def show(self, id):
 		ticket = db.session.query(Ticket).filter_by(id=id).first()
