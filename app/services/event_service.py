cf30c052c219b9dbac8717ca5259196923073ccb
andy-shi88 <shi88.andy@gmail.com>
2017-09-06 22:56:24 +0700

    #197 tweak event service response

--- app/services/event_service.py
+++ app/services/event_service.py
@@ -7,11 +7,32 @@ class EventService:
 
     def index(self):
         events = db.session.query(Event).all()
-        return Event().as_list(events)
+        _results = []
+        for event in events:
+            data = event.as_dict()
+            data['user'] = event.user.as_dict() if event.user else None
+            _results.append(data)
+        return {
+            'error': False,
+            'data': _results,
+            'message': 'Events succesfully retrieved'
+        }
 
     def show(self, id):
-        self.events_model = db.session.query(Event).filter_by(id=id).first()
-        return self.events_model.as_dict()
+        self.events_model = db.session.query(Event).filter_by(id=id)
+        if self.events_model.first() is None:
+            return {
+                'error': True, 
+                'data': None,
+                'message': 'event not found'
+            }
+        data = self.events_model.first().as_dict()
+        data['user'] = self.events_model.first().user.as_dict() if self.events_model.first().user else None
+        return {
+            'error': False,
+            'data': data,
+            'message': 'event retrieved succesfully'
+        }
 
     def create(self, payloads):
         try:
@@ -46,9 +67,9 @@ class EventService:
 
             new_event = {}
             if information:
-                new_event["information"] = information
+                new_event['information'] = information
             if title:
-                new_event["title"] = title
+                new_event['title'] = title
 
             self.events_model.update(new_event)
 
