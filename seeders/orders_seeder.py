a402df26b4ffe4b42e7b1cf7f00d3fe1a45ceefd
andy-shi88 <shi88.andy@gmail.com>
2017-09-07 15:03:29 +0700

    #199 temp fix for seeder

--- seeders/orders_seeder.py
+++ seeders/orders_seeder.py
@@ -1,4 +1,3 @@
-from faker import Faker
 from app.models.base_model import BaseModel
 from app.models import db
 from app.models.order import Order
@@ -17,13 +16,10 @@ class OrdersSeeder():
         """
         Create 10 orders seeds
         """
-        fake = Faker()
         users = BaseModel.as_list(db.session.query(User).all())
         for i in range(0, 10):
             user_id = random.choice(users)['id']
-            status = fake.sentence(
-                nb_words=6, variable_nb_words=True, ext_word_list=None
-            )
+            status = 'pending'
             new_order = Order()
             new_order.user_id = user_id
             new_order.status = status
