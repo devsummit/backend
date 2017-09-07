1c9720983aa669d3ec66a4fb03f57dd2fb8f92e0
erdivartanovich <erdivartanovich@gmail.com>
2017-09-07 10:41:47 +0700

    add additional user method

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -47,3 +47,29 @@ class UserController(BaseController):
             return BaseController.send_response_api(result['data'], 'user succesfully updated')
         else:
             return BaseController.send_error_api(None, result['data'])
+
+    @staticmethod
+    def add(request):
+        first_name = request.json['first_name'] if 'first_name' in request.json else None
+        last_name = request.json['last_name'] if 'last_name' in request.json else None
+        email = request.json['email'] if 'email' in request.json else None
+        username = request.json['username'] if 'username' in request.json else None
+        role_id = request.json['role_id'] if 'role_id' in request.json else None
+        if first_name and last_name and email and username and role_id:
+            payloads = {
+                'first_name': first_name,
+                'last_name': last_name,
+                'email': email,
+                'username': username,
+                'role_id': role_id
+
+            }
+        else:
+            return BaseController.send_error_api(None, 'field is not complete')
+
+        result = userservice.add(payloads)
+
+        if not result['error']:
+            return BaseController.send_response_api(result['data'], 'user succesfully added')
+        else:
+            return BaseController.send_error_api(None, result['data'])
