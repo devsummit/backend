97d424e4ab4065c957a28ce605d570c1eb3baf31
hafizhanindito <hafizpratama@gmail.com>
2017-09-08 17:00:19 +0700

    clean the requested fix for seed files

--- seeders/orders_seeder.py
+++ seeders/orders_seeder.py
@@ -31,21 +31,3 @@ class OrdersSeeder():
             db.session.commit()
 
         
-
-
-        
-
-        # fake = Faker()
-        # users = BaseModel.as_list(db.session.query(User).all())
-        # for i in range(0, 4):
-        #     user_id = random.choice(users)['id']
-        #     # The status of each order is either Reserved or Confirmed
-        #     status = random.choice(['Reserved', 'Confirmed'])
-        #     # status = fake.sentence(
-        #     #     nb_words=6, variable_nb_words=True, ext_word_list=None
-        #     # )
-        #     new_order = Order()
-        #     new_order.user_id = user_id
-        #     new_order.status = status
-        #     db.session.add(new_order)
-        #     db.session.commit()
