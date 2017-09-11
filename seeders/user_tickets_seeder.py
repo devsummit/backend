36de62c65db4a8946cadd1c13915559ccafb5e01
caudaganesh <caudaganesh@gmail.com>
2017-09-11 17:24:41 +0700

    fix lint

--- seeders/user_tickets_seeder.py
+++ seeders/user_tickets_seeder.py
@@ -1,6 +1,4 @@
-from app.models.base_model import BaseModel
 from app.models.user_ticket import UserTicket
-from app.models.payment import Payment
 from app.models import db
 
 
@@ -13,16 +11,16 @@ class UserTicketsSeeder():
 
     @staticmethod
     def run():
-        
+
         """
         Create UserTicket seeds
         """
 
         userid = [2, 1, 2, 3]
-       
-        for i in range(0,4):
+
+        for i in range(0, 4):
             new_user_ticket = UserTicket()
             new_user_ticket.user_id = userid[i]
-            
+
             db.session.add(new_user_ticket)
             db.session.commit()
