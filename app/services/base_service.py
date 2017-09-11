d6682072a3be71cfa031072bf167ebab41a80e92
andy-shi88 <shi88.andy@gmail.com>
2017-09-11 23:05:32 +0700

    #236 add base included and transform method

--- app/services/base_service.py
+++ app/services/base_service.py
@@ -10,7 +10,7 @@ class BaseService():
 
     def paginate(self, query):
         results = query.paginate(int(self.page), int(self.perpage), False)
-        paginate = {}
+        self.paginated = {}
         links = {}
 
         links['prev'] = (self.base_url + '?page=' + str(results.prev_num)) if results.prev_num else None
@@ -18,7 +18,26 @@ class BaseService():
         links['curr'] = self.base_url + '?page=' + str(self.page)
         links['total_items'] = self.total_items
 
-        paginate['data'] = BaseModel.as_list(results.items)
-        paginate['links'] = links
+        self.paginated['data'] = results.items
+        self.paginated['links'] = links
 
-        return paginate
+        return self.paginated
+
+    def include(self, fields):
+        _results = []
+        for item in self.paginated['data']:
+            data = item.as_dict()
+            for field in fields:
+                print(field)
+                data[field] = getattr(item, field).as_dict() if getattr(item, field) else None
+            _results.append(data)
+        self.paginated['data'] = _results
+        return self.paginated
+
+    def transform(self):
+        _results = []
+        for item in self.paginated['data']:
+            item = item.as_dict()
+            _results.append(item)
+        self.paginated['data'] = _results
+        return self.paginated
