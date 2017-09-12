97533fa39a052b9ae9ccdbe4c73014a043a5e63a
erdivartanovich <erdivartanovich@gmail.com>
2017-09-13 01:08:16 +0700

    define jinja global object in app __init__ file

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from app.routes.main import main
 from app.routes.api import api
 from app.routes.auth import auth
 
+# import some global configs
+from app.configs.constants import ROLE
+
 # db instance
 from app.models import db
 
@@ -22,5 +25,8 @@ def create_app(configuration):
 	app.register_blueprint(main)
 	app.register_blueprint(api, url_prefix=app.config['API_BASE_URL'])
 	app.register_blueprint(auth, url_prefix='/auth')
+
+	# define global object here to used across all templates
+	app.jinja_env.globals.update(roles=ROLE)
 
 	return app
