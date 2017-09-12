2fd3ef9337fb4b6df42a32964d221fd724303aff
andy-shi88 <shi88.andy@gmail.com>
2017-09-13 01:32:02 +0700

    #240 handle log for stage and type changes

--- app/services/sponsor_service.py
+++ app/services/sponsor_service.py
@@ -1,8 +1,10 @@
 import datetime
 from app.models import db
 from sqlalchemy.exc import SQLAlchemyError
+from app.configs.constants import SPONSOR_STAGES, SPONSOR_TYPES
 # import model class
 from app.models.sponsor import Sponsor
+from app.models.sponsor_interaction_log import SponsorInteractionLog
 from app.services.base_service import BaseService
 from app.builders.response_builder import ResponseBuilder
 
@@ -56,6 +58,25 @@ class SponsorService(BaseService):
     def update(self, id, payload):
         response = ResponseBuilder()
         sponsor = db.session.query(Sponsor).filter_by(id=id)
+        data = sponsor.first().as_dict() if sponsor.first() else None
+        if data is None:
+            return response.set_error(True).set_message('data not found').set_data(None).build()
+        if data['stage'] != payload['stage']:
+            log = SponsorInteractionLog()
+            _from = SPONSOR_STAGES[data['stage']] if data['stage'] else 'None'
+            _to = SPONSOR_STAGES[payload['stage']] if payload['stage'] else 'None'
+            log.description = 'Admin move stage from: ' + _from + ' to: ' + _to
+            log.sponsor_id = id
+            db.session.add(log)
+        
+        if (data['type'] != payload['type']):
+            log = SponsorInteractionLog()
+            _from = SPONSOR_TYPES[data['type']] if data['type'] else 'None'
+            _to = SPONSOR_TYPES[payload['type']] if payload['type'] else 'None'
+            log.description = 'Admin move stage from: ' + _from + ' to: ' + _to
+            log.sponsor_id = id
+            db.session.add(log)
+
         new_data = super().filter_update_payload(payload)        
         new_data['updated_at'] = datetime.datetime.now()
         sponsor.update(new_data)
