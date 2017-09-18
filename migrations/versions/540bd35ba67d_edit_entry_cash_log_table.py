46e6e1e83e4c17c08ceda65a98be7d06b561419f
hafizhanindito <hafizpratama@gmail.com>
2017-09-18 16:47:40 +0700

    Add migration for sources table

---
+++ migrations/versions/540bd35ba67d_edit_entry_cash_log_table.py
@@ -0,0 +1,24 @@
+"""edit_entry_cash_log_table
+
+Revision ID: 540bd35ba67d
+Revises: 3ff8278518a6
+Create Date: 2017-09-18 16:46:20.885067
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '540bd35ba67d'
+down_revision = '3ff8278518a6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
