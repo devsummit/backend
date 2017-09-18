a617ee55efec6204ac90532caa7e7587401a999f
andy-shi88 <shi88.andy@gmail.com>
2017-09-19 03:29:01 +0700

    #279 add type to notification

--- app/models/notification.py
+++ app/models/notification.py
@@ -7,7 +7,7 @@ class Notification(db.Model, BaseModel):
 
 	__tablename__ = 'notifications'
 
-	visible = ['id', 'message', 'sender_uid', 'receiver_uid', 'attachment', 'created_at', 'updated_at']
+	visible = ['id', 'message', 'sender_uid', 'receiver_uid', 'type', 'attachment', 'created_at', 'updated_at']
 
 	id = db.Column(db.Integer, primary_key=True)
 	message = db.Column(db.Text)
@@ -24,6 +24,7 @@ class Notification(db.Model, BaseModel):
 		nullable=False
 	)
 	receiver = db.relationship('User', foreign_keys=[receiver_uid])
+	type = db.Column(db.String)
 	created_at = db.Column(db.DateTime)
 	updated_at = db.Column(db.DateTime)
 
