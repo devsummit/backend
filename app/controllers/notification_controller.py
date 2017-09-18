f5f09a8af07b421cab52aa231e343c54bd74340b
andy-shi88 <shi88.andy@gmail.com>
2017-09-19 03:28:34 +0700

    #279 post notification

--- app/controllers/notification_controller.py
+++ app/controllers/notification_controller.py
@@ -5,29 +5,33 @@ from app.services import notificationservice
 class NotificationController(BaseController):
 
 	@staticmethod
-	def index(request):
-		notifications = notificationservice.get(request)
+	def index(request, user_id):
+		notifications = notificationservice.get(request, user_id)
 		return BaseController.send_response_api(notifications['data'], notifications['message'], {}, notifications['links'])
 
-	# @staticmethod
-	# def create(request, user_id):
-	# 	message = request.form['message'] if 'message' in request.form else None
-	# 	attachment = request.files['attachment'] if 'attachment' in request.files else None
-	# 	if message:
-	# 		payloads = {
-	# 			'message': message,
-	# 			'user_id': user_id,
-	# 			'attachment': attachment
-	# 		}
-	# 	else:
-	# 		return BaseController.send_error_api(None, 'field is not complete')
+	@staticmethod
+	def create(request, user):
+		message = request.json['message'] if 'message' in request.json else None
+		type = request.json['type'] if 'type' in request.json else None
+		attachment = request.json['attachment'] if 'attachment' in request.json else None
+		receiver = request.json['receiver'] if 'receiver' in request.json else None
+		if message and type:
+			payloads = {
+				'message': message,
+				'type': type,
+				'attachment': attachment,
+				'user': user,
+				'receiver': receiver
+			}
+		else:
+			return BaseController.send_error_api(None, 'field is not complete')
 
-	# 	result = feedservice.create(payloads)
+		result = notificationservice.create(payloads)
 
-	# 	if result['error']:
-	# 		return BaseController.send_error_api(result['data'], result['message'])
-	# 	else:
-	# 		return BaseController.send_response_api(result['data'], result['message'])
+		if result['error']:
+			return BaseController.send_error_api(result['data'], result['message'])
+		else:
+			return BaseController.send_response_api(result['data'], result['message'])
 
 	# @staticmethod
 	# def show(id):
