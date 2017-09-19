dbd8815f61062fc4957572af3e342131e288b61c
Aditia Pratama <aditiapratamagg@gmail.com>
2017-09-19 18:59:37 +0700

    add api for insert role attributes

---
+++ app/controllers/grantrole_controller.py
@@ -0,0 +1,80 @@
+from app.controllers.base_controller import BaseController
+from app.models.base_model import BaseModel
+from app.services import grantroleservice
+
+
+class GrantroleController(BaseController):
+
+    @staticmethod
+    def update(request, id):
+        role_id = request.json['role_id'] if 'role_id' in request.json else None
+        user_id = id
+        if role_id:
+            payload = {
+                'role_id': role_id
+            }
+            result = grantroleservice.update(payload, id)
+        else:
+            return BaseController.send_error_api(None, 'field is not complete')
+
+        if role_id == "1":
+            return BaseController.send_error(None, 'Admin cannot be grant')
+        elif role_id == "2":
+            points = request.json['includes']['points'] if 'points' in request.json['includes'] else 0
+            if points:
+                payloads = {
+                    'user_id': user_id,
+                    'points': points
+                }
+                result = grantroleservice.add_attendee(payloads)
+            else:
+                return BaseController.send_error_api(None, 'field is not complete')
+        elif role_id == "3":
+            stage_id = request.json['includes']['stage_id'] if 'stage_id' in request.json['includes'] else None
+            points = request.json['includes']['points'] if 'points' in request.json['includes'] else None
+            summary = request.json['includes']['summary'] if 'summary' in request.json['includes'] else None
+            if stage_id and points and summary:
+                payloads = {
+                    'user_id': user_id,
+                    'stage_id': stage_id,
+                    'points': points,
+                    'summary': summary
+                }
+                result = grantroleservice.add_booth(payloads)
+            else:
+                return BaseController.send_error_api(None, 'field is not complete')
+        elif role_id == "4":
+            job = request.json['includes']['job'] if 'job' in request.json['includes'] else None
+            summary = request.json['includes']['summary'] if 'job' in request.json['includes'] else None
+            information = request.json['includes']['information'] if 'information' in request.json['includes'] else None
+            if job and summary and information:
+                payloads = {
+                    'user_id': user_id,
+                    'job': job,
+                    'summary': summary,
+                    'information': information
+                }
+                result = grantroleservice.add_speaker(payloads)
+            else:
+                return BaseController.send_error_api(None, 'field is not complete')
+        elif role_id == "6":
+            information = request.json['includes']['information'] if 'information' in request.json['includes'] else None
+            institution = request.json['includes']['institution'] if 'institution' in request.json['includes'] else None
+            if information and institution:
+                payloads = {
+                    'user_id': user_id,
+                    'information': information,
+                    'institution': institution
+                }
+                result = grantroleservice.add_ambassador(payloads)
+            else:
+                return BaseController.send_error_api(None, 'field is not complete')
+
+        if not result['error']:
+            return BaseController.send_response_api(result['data'], 'User succesfully updated')
+        else:
+            return BaseController.send_error_api(None, result['data'])
+
+    @staticmethod
+    def get(request):
+        return grantroleservice.get(request)
--- app/services/grantrole_service.py
+++ app/services/grantrole_service.py
@@ -5,6 +5,10 @@ from sqlalchemy.exc import SQLAlchemyError
 from app.builders.response_builder import ResponseBuilder
 
 from app.models.user import User
+from app.models.attendee import Attendee
+from app.models.booth import Booth
+from app.models.speaker import Speaker
+from app.models.ambassador import Ambassador
 from app.models.base_model import BaseModel
 
 
@@ -23,3 +27,66 @@ class GrantroleService():
 
         except SQLAlchemyError as e:
             return response.set_data(e.orig.args).set_message('SQL error').set_error(True).build()
+    
+    
+    def add_attendee(self, payloads):
+        response = ResponseBuilder()
+        self.attendee = Attendee()
+        self.attendee.user_id = payloads['user_id']
+        self.attendee.points = payloads['points']
+        db.session.add(self.attendee)
+        try:
+            db.session.commit()
+            data = self.attendee.as_dict()
+            return response.set_data(data).build()
+        except SQLAlchemyError as e:
+            data = e.orig.args
+            return response.set_data(data).set_error(True).build()
+    
+    def add_booth(self, payloads):
+        response = ResponseBuilder()
+        self.booth = Booth()
+        self.booth.user_id = payloads['user_id']
+        self.booth.stage_id = payloads['stage_id']
+        self.booth.points = payloads['points']
+        self.booth.summary = payloads['summary']
+        db.session.add(self.booth)
+        try:
+            db.session.commit()
+            data = self.booth.as_dict()
+            return response.set_data(data).build()
+        except SQLAlchemyError as e:
+            data = e.orig.args
+            return response.set_data(data).set_error(True).build()
+
+    def add_speaker(self, payloads):
+        response = ResponseBuilder()
+        self.speaker = Speaker()
+        self.speaker.user_id = payloads['user_id']
+        self.speaker.job = payloads['job']
+        self.speaker.summary = payloads['summary']
+        self.speaker.information = payloads['information']
+        db.session.add(self.speaker)
+        try:
+            db.session.commit()
+            data = self.speaker.as_dict()
+            return response.set_data(data).build()
+        except SQLAlchemyError as e:
+            data = e.orig.args
+            return response.set_data(data).set_error(True).build()
+
+    def add_ambassador(self, payloads):
+        response = ResponseBuilder()
+        self.ambassador = Ambassador()
+        self.ambassador.user_id = payloads['user_id']
+        self.ambassador.informations = payloads['infrormation']
+        self.ambassador.institution = payloads['institution']
+        db.session.add(self.ambassador)
+        try:
+            db.session.commit()
+            data = self.ambassador.as_dict()
+            return response.set_data(data).build()
+        except SQLAlchemyError as e:
+            data = e.orig.args
+            return response.set_data(data).set_error(True).build()
+            
