049fae8ce5ec3f6692da0a6e0a37282c4b592449
erdivartanovich <erdivartanovich@gmail.com>
2017-09-21 02:36:06 +0700

    add log interaction service

--- app/services/speaker_candidate_service.py
+++ app/services/speaker_candidate_service.py
@@ -2,6 +2,7 @@ import datetime
 from app.models import db
 from sqlalchemy.exc import SQLAlchemyError
 from app.models.speaker_candidate import SpeakerCandidate
+from app.models.speaker_candidate_log import SpeakerCandidateLog
 
 
 class SpeakerCandidateService():
@@ -14,7 +15,7 @@ class SpeakerCandidateService():
             _candidates.append(data)
         return {
             'data': _candidates,
-            'message': 'speaker candidate retrieved successfully',
+            'message': 'speaker candidates retrieved successfully',
             'error': False
         }
 
@@ -73,6 +74,38 @@ class SpeakerCandidateService():
                 'error': False,
                 'data': data,
                 'included': []
+            }
+        except SQLAlchemyError as e:
+            return {
+                'error': True,
+                'data': e.orig.args
+            }
+
+    def show_logs(self, id):
+        logs = db.session.query(SpeakerCandidateLog).filter_by(
+            candidate_id=id).all()
+        _logs = []
+        for log in logs:
+            data = log.as_dict()
+            _logs.append(data)
+        return {
+            'data': _logs,
+            'message': 'logs retrieved successfully',
+            'error': False
+        }
+
+    def create_log(self, payloads):
+        self.model_candidate_log = SpeakerCandidateLog()
+        for key in payloads:
+            setattr(self.model_candidate_log, key, payloads[key])
+        db.session.add(self.model_candidate_log)
+        try:
+            db.session.commit()
+            data = self.model_candidate_log.as_dict()
+            return {
+                'error': False,
+                'data': data,
+                'message': 'Interaction log created succesfully'
             }
         except SQLAlchemyError as e:
             return {
