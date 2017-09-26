cbc914a965c6fa60caa4e2d3a71debd896ac63c2
andy-shi88 <shi88.andy@gmail.com>
2017-09-26 15:07:41 +0700

    tweak ticket transfer to receive email and username

--- app/services/ticket_transfer_service.py
+++ app/services/ticket_transfer_service.py
@@ -1,8 +1,11 @@
 import datetime
 from app.models import db
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import or_
 # import model class
 from app.models.user_ticket import UserTicket
+from app.models.user import User
+from app.builders.response_builder import ResponseBuilder
 from app.models.ticket_transfer_log import TicketTransferLog
 
 
@@ -20,20 +23,20 @@ class TicketTransferService():
 
         return transferslogs
 
-    def transfer(self, user_id, user_ticket_id, receiver_id):
-        if (user_id == '' or user_ticket_id == '' or receiver_id == ''):
-            return {
-                'error': True,
-            				'data': 'sender_id, user_ticket_id or receiver_id is not valid'
-            }
-
+    def transfer(self, user_id, user_ticket_id, receiver):
+        response = ResponseBuilder()
         userticket = db.session.query(UserTicket).filter_by(id=user_ticket_id)
-        if userticket.count() <= 0:
-            return {
-                'error': True,
-                'data': 'ticket not found'
-            }
+        if userticket.first() is None:
+            return response.set_error(True).set_data(None).set_message('ticket not found').build()
 
+        if userticket.first().as_dict()['user_id'] != user_id:
+            return response.set_error(True).set_data(None).set_message('this ticket is not yours').build()
+
+        receiver_raw = db.session.query(User).filter(or_(User.username.like(receiver), User.email.like(receiver))).first()
+
+        if receiver_raw is None:
+            return response.set_error(True).set_data(None).set_message('receiver could not be found').build()
+        receiver_id = receiver_raw.as_dict()['id']
         try:
             # update user ticket
             userticket.update({
@@ -52,13 +55,8 @@ class TicketTransferService():
 
             db.session.commit()
             data = transferlog.as_dict()
-            return {
-                'error': False,
-            				'data': data
-            }
+            return response.set_data(data).set_message('ticket transfered successfully').build()
+
         except SQLAlchemyError as e:
             data = e.orig.args
-            return {
-                'error': True,
-                'data': data
-            }
+            return response.set_data(None).set_message(data).set_error(True).build()
