6f9a817c6180c1c99bf5f9f7fd477c5ce3fec4bb
andy-shi88 <shi88.andy@gmail.com>
2017-09-29 12:20:32 +0700

    switch back the port

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ from app.models import socketio
 if __name__ == "__main__":
 	app = create_app(settings)
 	socketio.init_app(app)
-	socketio.run(app, host='0.0.0.0', port=8081)
+	socketio.run(app, host='0.0.0.0', port=5000)
