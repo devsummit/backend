1cc738f60a7f8fc4ecb26f3b081e425e12d72ae5
andy-shi88 <shi88.andy@gmail.com>
2017-10-04 17:21:08 +0700

    handle old null type feeds

--- app/services/base_service.py
+++ app/services/base_service.py
@@ -53,12 +53,11 @@ class BaseService():
         _results = []
         for item in self.paginated['data']:
             data = item.as_dict()
-            if item.type:
-                if 'user' in item.type:
-                    data['user'] = item.user.include_photos().as_dict()
-                elif 'sponsor' in item.type:
-                    sponsor = db.session.query(Sponsor).filter_by(id=item.sponsor_id).first()        
-                    data['user'] = sponsor.as_dict()
+            if 'user' in item.type or item.type is None:
+                data['user'] = item.user.include_photos().as_dict()
+            elif 'sponsor' in item.type:
+                sponsor = db.session.query(Sponsor).filter_by(id=item.sponsor_id).first()        
+                data['user'] = sponsor.as_dict()
             _results.append(data)
         self.paginated['data'] = _results
         return self.paginated
