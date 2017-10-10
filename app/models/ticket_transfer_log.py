5ce3843d1c57790254cdc2acc503f43ca3f9ad1b
hafizhanindito <hafizpratama@gmail.com>
2017-10-10 16:52:33 +0700

    add foreign key parameter to model in receiver and sender to refer user_id

--- app/models/ticket_transfer_log.py
+++ app/models/ticket_transfer_log.py
@@ -9,13 +9,21 @@ class TicketTransferLog(db.Model, BaseModel):
 	# table name
 	__tablename__ = 'ticket_transfer_logs'
 	# displayed fields
-	visible = ['user_ticket_id', 'sender_user_id', 'receiver_user_id']
+	visible = ['user_ticket_id', 'sender_user_id', 'receiver_user_id', 'sender', 'receiver', 'created_at', 'updated_at']
 
 	# columns definitions
 	id = db.Column(db.Integer, primary_key=True)
 	user_ticket_id = db.Column(db.Integer)
-	sender_user_id = db.Column(db.Integer)
-	receiver_user_id = db.Column(db.Integer)
+	sender_user_id = db.Column(
+		db.Integer, 
+		db.ForeignKey('users.id'),
+		nullable=False)
+	receiver_user_id = db.Column(
+		db.Integer, 
+		db.ForeignKey('users.id'),
+		nullable=False)
+	sender = db.relationship("User", foreign_keys=[sender_user_id])
+	receiver = db.relationship("User", foreign_keys=[receiver_user_id])
 	created_at = db.Column(db.DateTime)
 	updated_at = db.Column(db.DateTime)
 
