4f0bf0ba8a13d95fb29b14c589380705eaa6d780
andy-shi88 <shi88.andy@gmail.com>
2017-10-15 18:44:24 +0700

    fix unauthorized response

--- app/middlewares/authentication.py
+++ app/middlewares/authentication.py
@@ -2,7 +2,7 @@ from app.models import db
 from sqlalchemy.exc import SQLAlchemyError
 from app.models.access_token import AccessToken
 from functools import wraps
-from flask import jsonify, request, current_app
+from flask import jsonify, request, current_app, Response, json
 from itsdangerous import (TimedJSONWebSignatureSerializer 
 	as Serializer, BadSignature, SignatureExpired
 	)
@@ -14,25 +14,19 @@ def token_required(f):
 	@wraps(f)
 	def decorated(*args, **kwargs):
 		if 'Authorization' not in request.headers:
-			return jsonify({'message': 'token is missing'})
+			return Response(json.dumps({'message': 'Authorization header is missing'}), status=401, mimetype='application/json')
 		else:
 			token = request.headers['Authorization']
 		s = Serializer(current_app.config['SECRET_KEY'])
 		try:
 			s.loads(token)
 		except SignatureExpired:
-			return jsonify({
-				'message': 'token is expired',
-				'expired': True
-				})
+			return Response(json.dumps({'message': 'Token is missing'}), status=401, mimetype='application/json')
 		except BadSignature:
-			return jsonify({
-				'message': 'token is invalid',
-				'invalid': True
-				})
+			return Response(json.dumps({'message': 'Token is invalid'}), status=401, mimetype='application/json')
 		kwargs['user'] = User.verify_auth_token(token)
 		token = db.session.query(AccessToken).filter_by(access_token=token).first()
 		if token is None:
-			return jsonify({'message': 'user is not logged in, try loggin in again.'})
+			return Response(json.dumps({'message':'User is not logged in'}), status=401, mimetype='application/json')
 		return f(*args, **kwargs)
 	return decorated
