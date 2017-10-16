64a2932e4f3f341540b0419d11c24f103740774c
erdivartanovich <erdivartanovich@gmail.com>
2017-10-16 15:31:01 +0700

    finalizing functionality

--- app/static/src/payment_verification/verification-model.js
+++ app/static/src/payment_verification/verification-model.js
@@ -1,5 +1,6 @@
 var Verification = {
     list: [],
+    current: {},
     loadList: function() {
         return m.request({
             method: "GET",
@@ -11,6 +12,22 @@ var Verification = {
         })
         .then(function(result) {
             Verification.list = result.data
+        })
+    },
+    save: function() {
+        return m.request({
+            method: "POST",
+            url: "api/v1/order-verification/" + Verification.current.id + "/verify",
+            headers: {
+                Authorization: dsa.acess_token()
+            },
+            withCredentials: true,
+        })
+        .then(function(result) {
+            if(result.meta.success) {
+                alert('Verification success tiket have been created!')
+                Verification.current.is_used = true
+            }
         })
     }
 }
