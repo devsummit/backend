deab14d3a8b57476fd0895bea9d2e12f34da7671
hafizhanindito <hafizpratama@gmail.com>
2017-10-17 18:09:24 +0700

    initialize migration and model for hackaton_team and hackaton_user

---
+++ app/models/hacker_team.py
@@ -0,0 +1,31 @@
+import datetime
+
+from app.models.base_model import BaseModel
+from app.models import db
+
+
+class HackerTeam(db.Model, BaseModel):
+    # table name
+    __tablename__ = 'hacker_teams'
+    # displayed fields
+    visible = ['id', 'name', 'logo', 'project_name', 'project_url', 'theme', 'created_at', 'updated_at']
+
+    # columns definitions
+    id = db.Column(
+        db.String, 
+        primary_key=True
+    )
+    name = db.Column(
+        db.String(60),
+        nullable=False
+    )
+    logo = db.Column(db.String(180))
+    project_name = db.Column(db.String(60))
+    project_url = db.Column(db.String(250))
+    theme = db.Column(db.String(60))
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    def __init__(self):
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
