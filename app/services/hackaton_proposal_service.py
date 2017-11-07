103769345827e6c0ae48c10ee12d9b540451b3e9
andy-shi88 <shi88.andy@gmail.com>
2017-11-07 14:44:07 +0700

    add route to deny and verify proposal

--- app/services/hackaton_proposal_service.py
+++ app/services/hackaton_proposal_service.py
@@ -1,22 +1,31 @@
 import datetime
-from flask import current_app
+from flask import current_app, request
 from app.models import db
-from app.models.hackaton_proposals import HackatonProposal
-from app.services.helper import Helper 
 from sqlalchemy.exc import SQLAlchemyError
+from app.configs.constants import ROLE
+from app.services.helper import Helper
+from app.services.fcm_service import FCMService
+from app.services.order_verification_service import OrderVerificationService
 from app.services.base_service import BaseService
 from app.builders.response_builder import ResponseBuilder
+from app.models.hackaton_proposals import HackatonProposal
+from app.models.order import Order
 
 
 class HackatonProposalService(BaseService):
 
+	def __init__(self):
+		self.orderverificationservice = OrderVerificationService()
 
 	def get(self, status):
 		response = ResponseBuilder()
 		results = db.session.query(HackatonProposal).filter_by(status=status).all()
 		_results = []
 		for result in results:
-			_results.append(result)
+			data = result.as_dict()
+			data['order'] = result.order.as_dict()
+			data['user'] = result.order.user.as_dict()
+			_results.append(data)
 		return response.set_data(_results).build()
 
 	def create(self, payloads):
@@ -33,10 +42,16 @@ class HackatonProposalService(BaseService):
 			data = e.orig.args
 			return response.set_data(data).set_error(True).build()		
 
+	def check_hackaton_proposal_exist(self, user_id):
+		order_ids = db.session.query(Order.id).filter_by(user_id=user_id)
+		hackaton_proposal = db.session.query(HackatonProposal).join(Order).filter(Order.user_id == user_id).first()
+		if hackaton_proposal:
+			return True
+		return False
 
 	def deny(self, payloads):
 		response = ResponseBuilder()
-		hackaton_proposal = db.session.query(HackatonProposal).filter_by(id=payloads['id'])
+		hackaton_proposal = db.session.query(HackatonProposal).filter_by(order_id=payloads['order_id'])
 		if hackaton_proposal.first() is None:
 			return response.set_error(True).set_data(None).set_message('proposal not found').build()
 		hackaton_proposal.update({
@@ -45,7 +60,27 @@ class HackatonProposalService(BaseService):
 		})
 		try:
 			db.session.commit()
+			hackatonprop = hackaton_proposal.first()
+			receiver = hackatonprop.order.user_id
+			send_notification = FCMService().send_single_notification('Hackaton Status', 'Your proposal to join our hackaton has just been rejected, as this may be dissapointing to you, you can still purchase our ticket as attendee. See you there.', receiver, ROLE['admin'])
 			return response.set_data(None).set_message('proposal succesfully denied').build()
+		except SQLAlchemyError as e:
+			data = e.orig.args
+			return response.set_data(data).set_error(True).build()
+
+	def verify(self, payloads):
+		response = ResponseBuilder()
+		hackaton_proposal = db.session.query(HackatonProposal).filter_by(order_id=payloads['order_id'])
+		if hackaton_proposal.first() is None:
+			return response.set_error(True).set_data(None).set_message('proposal not found').build()
+		hackaton_proposal.update({
+			'updated_at': datetime.datetime.now(),
+			'status': 'verified'
+		})
+		try:
+			db.session.commit()
+			self.orderverificationservice.admin_verify(payloads['order_id'], request)
+			return response.set_data(None).set_message('hackaton proposal succesfully accepted').build()
 		except SQLAlchemyError as e:
 			data = e.orig.args
 			return response.set_data(data).set_error(True).build()
