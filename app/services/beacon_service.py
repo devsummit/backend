9314f1c084aaeb7e8233c3fd40f5356380994508
andy-shi88 <shi88.andy@gmail.com>
2017-11-14 16:19:36 +0700

    implement fetch mapping

--- app/services/beacon_service.py
+++ app/services/beacon_service.py
@@ -4,6 +4,10 @@ from sqlalchemy.exc import SQLAlchemyError
 from app.builders.response_builder import ResponseBuilder
 # import model class
 from app.models.beacon import Beacon
+from app.models.sponsor import Sponsor
+from app.models.speaker import Speaker
+from app.models.booth import Booth
+from app.models.booth_gallery import BoothGallery
 from app.models.beacon_map_version import BeaconMapVersion
 
 
@@ -82,6 +86,9 @@ class BeaconService():
 			data = e.orig.args
 			return response.set_data(data).set_error(True).set_message('sql error').build()
 
+	def get_current_version(self):
+		return db.session.query(BeaconMapVersion).first().version
+
 	def delete(self, id):
 		response = ResponseBuilder()
 		self.model_beacon = db.session.query(Beacon).filter(Beacon.id == id)
@@ -93,3 +100,45 @@ class BeaconService():
 		else:
 			data = 'data not found'
 			return response.set_data(data).set_error(True).set_message('sql error').build()
+
+	def fetch_mapping(self, version):
+		response = ResponseBuilder()
+		result = []
+		current_version = self.get_current_version()
+		if version == current_version:
+			return response.set_data({'newest': True}).set_message('your beacon map version is already the newest').build()
+		# fetch all row from beacon map
+		beacons = db.session.query(Beacon).all()
+		# for each beacon map include all data neccessary according to their type and type id
+		for beacon in beacons:
+			data = beacon.as_dict()
+			data['details'] = self.include_data(beacon)
+			result.append(data)
+		# fetch map here
+		# return response
+		return response.set_data(result).set_message('You have updated your beacon map to version: %s' %current_version).build()
+
+
+	def include_data(self, beacon):
+		result = {}
+		if beacon.type == 'exhibitor':
+			exhibitor = db.session.query(Booth).filter(Booth.id == beacon.type_id).first()
+			gallery = self.fetch_booth_gallery(exhibitor.id)
+			result['exhibitor'] = exhibitor.as_dict()
+			result['exhibitor']['gallery'] =[]
+			for image in gallery:
+				result['exhibitor']['gallery'].append(image.as_dict())
+		elif beacon.type == 'sponsor':
+			result['sponsor'] = db.session.query(Sponsor).filter(Sponsor.id == beacon.type_id).first().as_dict()
+		elif beacon.type == 'speaker':
+			speaker = db.session.query(Speaker).filter(Speaker.id == beacon.type_id).first()
+			result['speaker'] = speaker.as_dict()
+			result['speaker']['user'] = speaker.user.include_photos().as_dict()
+		elif beacon.type == 'other':
+			pass
+		elif beacon.type == 'entrance':
+			pass
+		return result
+
+	def fetch_booth_gallery(self, booth_id):
+		return db.session.query(BoothGallery).filter(BoothGallery.booth_id == booth_id).all()
